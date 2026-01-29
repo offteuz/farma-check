7fd212842cf90a4890b3f747af65e04cd9cf373d
Matheus Jesus de Souza <offteuz@gmail.com>
2026-01-29 19:19:23 -0300

    feat: primeiro commit

--- src/main/java/com/fiap/feedbacksystem/FeedbacksystemApplication.java
+++ src/main/java/com/fiap/feedbacksystem/FeedbacksystemApplication.java
@@ -1,6 +1,5 @@
 package com.fiap.feedbacksystem;
 
-import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,10 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class FeedbacksystemApplication {
 
 	public static void main(String[] args) {
-		// Ativa o Azure Application Insights por código.
-		// Precisa vir antes do SpringApplication.run().
-		ApplicationInsights.attach();
-		
 		SpringApplication.run(FeedbacksystemApplication.class, args);
 	}
 
--- src/main/java/com/fiap/feedbacksystem/repository/AulaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fiap.feedbacksystem.repository;
-
-import com.fiap.feedbacksystem.model.entity.Aula;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AulaRepository extends JpaRepository<Aula, Integer> {
-}
