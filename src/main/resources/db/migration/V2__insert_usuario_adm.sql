daed79172ba709970fb473193bb49b4c334a6342
Matheus Jesus de Souza <offteuz@gmail.com>
2026-02-13 05:40:13 -0300

    fix: adiciona senha criptografada ao migration migration de criacao de usuario administrador

--- src/main/resources/db/migration/V2__insert_usuario_adm.sql
+++ src/main/resources/db/migration/V2__insert_usuario_adm.sql
@@ -4,4 +4,4 @@
 
 -- Usuario
 INSERT INTO USUARIO (id, nome, email, senha, tipo_usuario, unidade_id, criacao, ultima_modificacao)
-VALUES ( nextval('usuario_sequence'), 'Admin', 'admin@farmacheck.com', 'admin', 'ADMINISTRADOR', null, NOW(), NOW());
+VALUES ( nextval('usuario_sequence'), 'Admin', 'admin@farmacheck.com', '$2a$10$8WCWreHSym31HI/EZB1sTOKLxvb04KMUUSogtD8lIQDKknjIyS6fa', 'ADMINISTRADOR', null, NOW(), NOW());
